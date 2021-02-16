b532123c4b8eafc027eff4331e2a0dec24164463
Sam Hellawell <sshellawell@gmail.com>
2021-02-16 01:00:37 +0000

    implement /call route for queries

--- src/services/CallService.js
+++ src/services/CallService.js
@@ -2,8 +2,13 @@ import RosettaSDK from 'rosetta-node-sdk';
 
 import {
   ERROR_PARSE_INTENT,
+  ERROR_POLKADOT_ERROR,
   throwError,
 } from '../helpers/error-types';
+
+import {
+  getNetworkApiFromRequest,
+} from '../helpers/connections';
 
 const Types = RosettaSDK.Client;
 
@@ -19,9 +24,37 @@ const Types = RosettaSDK.Client;
 const call = async (params) => {
   const { callRequest } = params;
   const { method, parameters } = callRequest;
-  console.log('callRequest', callRequest);
+  const api = await getNetworkApiFromRequest(callRequest);
+
+  // Get method function from string
+  const methodSplit = method.split('.');
+  const methodType = methodSplit[0];
+  let methodFn = api[methodType];
+  for (let i = 1; i < methodSplit.length; i++) {
+    methodFn = methodFn[methodSplit[i]];
+  }
+
+  if (!methodFn) {
+    throwError(ERROR_PARSE_INTENT);
+  }
+
+  const isQuery = methodType === 'query';
+  const { blockHash, args = [] } = parameters;
+
+  try {
+    const extrinsic = (blockHash && isQuery) ? methodFn.at(blockHash, ...args) : methodFn(...args);
+    if (isQuery) {
+      const result = (await extrinsic).toJSON();
+      return {
+        result,
+        idempotent: isQuery,
+      };
+    }
+  } catch (e) {
+    throwError(ERROR_POLKADOT_ERROR, e.toString());
+  }
+
   throwError(ERROR_PARSE_INTENT);
-  return {};
 };
 
 module.exports = {
